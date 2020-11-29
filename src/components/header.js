13487ed065c8633a124e9d87e2e38e191e9def93
AbdulhakimZatar <hakimzatar@gmail.com>
2020-11-29 23:02:21 +0200

    fix

--- src/components/header.js
+++ src/components/header.js
@@ -3,7 +3,7 @@ import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
-import { red } from "@material-ui/core/colors";
+// import { red } from "@material-ui/core/colors";
 // import IconButton from '@material-ui/core/IconButton';
 // import MenuIcon from '@material-ui/icons/Menu';
 
